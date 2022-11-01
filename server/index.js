77d8270e47d238e0a03961b803a7d846946c9911
zeshan <zeshan.tawa98@gmail.com>
2022-11-01 15:45:46 -0400

    forgot to save...take 2

--- server/index.js
+++ server/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
 const PORT = 3001;
 
+const db = mysql.createConnection({  //establish connection to db
+    user: 'root', 
+    host: 'localhost',
+    password: 'password..',
+    datbase: 'PassMang'
+})
 
 app.get("/", (req, res) => {
     res.send("Hello World");
