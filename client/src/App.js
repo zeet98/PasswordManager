46b3cee3849bea215e8024b8fdeea3c959a2ffa1
zeshan <zeshan.tawa98@gmail.com>
2022-11-02 15:41:28 -0400

    use state added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
 
 import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [password, setPassword] = useState('')
+  const [url, setUrl] = useState('')
+
   return <div className="App">
     <div className="AddingPassword">
-      <input type="text" placeholder="Ex. password 123" />
-      <input type="text" placeholder="Ex. Facebook" />
+      <input 
+        type="text"
+        placeholder="Ex. password 123" 
+        onChange={(event)=>{
+          setPassword(event.target.value);
+        }}
+      />
+      <input 
+        type="text" 
+        placeholder="Ex. Facebook" 
+        onChange={(event)=>{
+          setUrl(event.target.value);
+        }}
+        />
       <button> Add Password </button>
     </div>
   </div>;
